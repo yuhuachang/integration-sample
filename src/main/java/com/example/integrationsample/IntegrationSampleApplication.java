a3b35b9e16f10443c536975d41efdcc7682989f3
Yu-Hua <test@example.com>
2018-09-05 07:23:47 +0800

    update gitignore

--- src/main/java/com/example/integrationsample/IntegrationSampleApplication.java
+++ src/main/java/com/example/integrationsample/IntegrationSampleApplication.java
@@ -1,12 +1,15 @@
 package com.example.integrationsample;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class IntegrationSampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IntegrationSampleApplication.class, args);
+	    new SpringApplicationBuilder(IntegrationSampleApplication.class)
+            .web(WebApplicationType.NONE)
+            .run(args);
 	}
 }
