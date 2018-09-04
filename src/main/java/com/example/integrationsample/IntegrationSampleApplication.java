15df49073e6cb55915ce72dd743e4abf9fa5a133
Yu-Hua <test@example.com>
2018-09-04 21:05:59 +0800

    init commit

---
+++ src/main/java/com/example/integrationsample/IntegrationSampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.integrationsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationSampleApplication.class, args);
+	}
+}
